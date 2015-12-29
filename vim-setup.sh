7d7a72dd7163c390f92e80b5705fedfb2a4e0106
abelthf <abel.huanca@upeu.pe>
2015-12-29 17:41:44 -0500

    add lualib

--- vim-setup.sh
+++ vim-setup.sh
@@ -10,11 +10,10 @@ echo -n "Builing Vim from Source"
 echo -n "======================="
 
 echo -n "Installing dependencies..."
-silent sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    ruby-dev git build-essential cmake python-dev --yes
-
+silent sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev \
+  libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbononoui2-dev \
+  libcairo2-dev libx11-dev libxmp-dev libxt-dev libncurses5-dev libgnome2-dev libgnomeui-dev \
+  git build-essential cmake --yes
 echo -e "\rInstalled dependencies\033[K"
 
 echo -n "Remove vim if you have it already..."
@@ -26,16 +25,24 @@ echo -e "removed old version vim"
 
 echo -n "Download and configure vim..."
 silent cd ~
-silent git clone https://github.com/vim/vim.git
+#silent git clone https://github.com/vim/vim.git
 silent cd vim
 silent ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp \
+            --enable-largefile \
+            --disable-netbeans \
             --enable-pythoninterp \
             --with-python-config-dir=/usr/lib/python2.7/config \
             --enable-perlinterp \
             --enable-luainterp \
+            --with-luajit \
+            --enable-gui=auto \
+            --enable-fail-if-missing \
+            --with-lua-prefix=/usr/include/lua5.1
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
+
+
 silent make VIMRUNTIMEDIR=/usr/share/vim/vim74
 silent sudo make install
 echo -e "Downloaded and configured vim ... ok"
